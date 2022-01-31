7ac1e63d194b778998dcc385d43c01245d0f5845
Md-omar-faruke-programming-hero <farukeomar@gmail.com>
2022-01-31 18:12:38 +0600

    .

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const cors= require('cors')
+const cors = require('cors')
 const { MongoClient } = require('mongodb');
-const ObjectId= require('mongodb').ObjectId
-const app = express()
-const port = process.env.PORT||5000
+const ObjectId = require('mongodb').ObjectId;
+const app = express();
+const port = process.env.PORT || 5000;
 
 
 // middleware
@@ -15,98 +15,120 @@ app.use(express.json())
 const uri = "mongodb+srv://myNodeDbuser:E4f6fskQSC1Svo0Z@cluster0.eqb98.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run(){
+async function run() {
 
-    try{
-        await client.connect()
+  try {
+    await client.connect()
 
-        const database = client.db("spraxa");
-        const memberCollection = database.collection("allMember");
+    const database = client.db("spraxa");
+    const memberCollection = database.collection("allMember");
 
-
-        // post api
-        app.post('/member',async(req,res)=>{
-            const newMember= req.body
-            const result= await memberCollection.insertOne(newMember)
-           
-            res.json(result)
-
-        })
+    const userCollection = database.collection("users")
 
 
-        // get api for all member
-        app.get('/allMember',async(req,res)=>{
-          let filter={isDeleted:parseInt(req.query.isDeleted)};       
-         
-            const cursor= memberCollection.find(filter)
-            const result= await cursor.toArray()
-            res.send(result)
-            console.log(req.body)
-            
-        })
+    // post api
+    app.post('/member', async (req, res) => {
+      const newMember = req.body
+      const result = await memberCollection.insertOne(newMember)
+      res.json(result)
 
-        // get api for single mebere
-        app.get('/allMember/:id',async(req,res)=>{
-            const id= req.params.id;
-            const query= {_id:ObjectId(id)}
-            const result= await memberCollection.findOne(query)
-            res.send(result)
-        })
+    })
 
-        // delete member list
-        app.put('/allMembers/:id',async(req,res)=>{
-            const id=req.params.id;
-            const updatedUser=req.body
-            console.log(updatedUser);
-            const query={_id:ObjectId(id)}
-            const options = { upsert: true };
-            const doc = {
-                $set: {
-                  isDeleted:updatedUser.isDeleted,
-                  
-                },
-              };
-              const result= await memberCollection.updateOne(query,doc,options)
-            console.log(result);
-            res.json(result)
-            
-            
-        })
 
-        // update api
-        app.put('/allMember/:id',async(req,res)=>{
-            const id=req.params.id;
-            const updatedUser=req.body
-            const query= {_id:ObjectId(id)} 
-            const options = { upsert: true };
-            const doc = {
-                $set: {
-                  name:updatedUser.name,
-                  address:updatedUser.address,
-                  email:updatedUser.address,
-                  contact:updatedUser.contact
-                },
-              };
+    // get api for all member
+    app.get('/allMember', async (req, res) => {
+      let filter = { isDeleted: parseInt(req.query.isDeleted) };
 
-            const result= await memberCollection.updateOne(query,doc,options)
-            console.log(result);
-            res.json(result)
-        })
+      const cursor = memberCollection.find(filter)
+      const result = await cursor.toArray()
+      res.send(result)
+      console.log(req.body)
 
-        app.post('/signup', async(req, res)=> {
-          const {username, password} = req.params;
-          //here we need connectivity with firebase 
-          //which will handle our signup
-          //if user is successfully created, then we will send success message otherwise we will send failure message to user.
-          //res.json({ success: true, message: 'User added successfully'})
-        })
+    })
+    // single member api
+    app.get('/allMember/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const result = await memberCollection.findOne(query)
+      res.send(result)
+    })
 
-        
+    // delete member list
+    app.put('/allMembers/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body
+      console.log(updatedUser);
+      const query = { _id: ObjectId(id) }
+      const options = { upsert: true };
+      const doc = {
+        $set: {
+          isDeleted: updatedUser.isDeleted,
 
-          
-    } finally{
-        // await client.close()
-    }
+        },
+      };
+      const result = await memberCollection.updateOne(query, doc, options)
+      console.log(result);
+      res.json(result)
+
+
+    })
+
+    // update api
+    app.put('/allMember/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body
+      const query = { _id: ObjectId(id) }
+      const options = { upsert: true };
+      const doc = {
+        $set: {
+          name: updatedUser.name,
+          address: updatedUser.address,
+          email: updatedUser.address,
+          contact: updatedUser.contact
+        },
+      };
+
+      const result = await memberCollection.updateOne(query, doc, options)
+      console.log(result);
+      res.json(result)
+    })
+
+
+    // register user info post api
+
+    app.post('/createUser', async (req, res) => {
+
+      const newUser = req.body
+      console.log(newUser)
+      const result = await userCollection.insertOne(newUser)
+      res.json(result)
+
+
+    })
+
+    app.post('/login', async (req, res) => {
+      const { email, password } = req.body;
+      const query = { email: email, pass1: password }
+      const result = await userCollection.findOne(query)
+      if (result) {
+        res.send({ status: 200, message: "user logged in successfully" })
+      }
+      else {
+        res.send({ message: "User not found", status: 404 })
+      }
+
+    })
+
+
+
+
+
+
+
+
+  } finally {
+    // await client.close()
+  }
 
 }
 run().catch(console.dir)
@@ -116,6 +138,8 @@ run().catch(console.dir)
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+// app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
